31f0183aa0e6e581bf3217d057dd8b691ca75078
ismlop87-tech <ismlop.87@gmail.com>
2026-04-15 19:33:30 -0700

    Agregue preguntas 8 9 y 10

--- Practica_evaluatoria/Evaluacion1.scala
+++ Practica_evaluatoria/Evaluacion1.scala
@@ -6,3 +6,13 @@ df.printSchema()
 df.show(5)
 df.describe().show()
 val df2 = df.withColumn("HV Ratio",df("High")/df("Volume"))
+df.orderBy($"Open".desc).select("Date", "Open").show(1)
+//La columna “Close” representa el precio de cierre de la acción al final de la jornada bursátil.//
+
+//Es importante porque://
+
+//Refleja el último valor al que se negoció la acción ese día.//
+//Se usa como referencia principal para análisis financiero.//
+//Muchos indicadores técnicos se basan en este valor.//
+//Permite comparar rendimiento entre días.//
+df.select(max("Volume"),min("Volume")).show()
